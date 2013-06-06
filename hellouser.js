1cf34dd47394e6821d68e36e6152f741d246a9b5
akasharora <akasharora90@gmail.com>
2013-06-06 10:24:51 +0530

    hellouser

---
+++ hellouser.js
@@ -0,0 +1,12 @@
+var msg=document.getElementById("hello");
+var fname=prompt("please enter your first name");
+var lname=prompt("please enter your last name")
+var op="";
+if(fname&&lname){
+var op="hello " + fname + " "+lname+ ", how are you?" ;
+
+}
+else{
+op="please try again and enter your full name";
+}
+msg.textContent=op;
