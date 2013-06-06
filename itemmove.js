150c8b21a899f60c28878a631b44b77187d8e23d
akasharora <akasharora90@gmail.com>
2013-06-06 16:27:16 +0530

    itemmove

---
+++ itemmove.js
@@ -0,0 +1,28 @@
+function ItemAdd(direction)
+{
+var lsel=document.getElementById("lselect");
+var rsel=document.getElementById("rselect");
+var temp;
+var selected=[];
+
+if(direction)
+{
+temp=rsel;
+rsel=lsel;
+lsel=temp;
+}
+
+for(var i in lsel.options)
+{
+if(lsel.options[i].selected)
+{
+rsel.options.add(new Option(lsel.options[i].text, lsel.options[i].value));
+selected.unshift(i);
+}
+}
+for(i in selected)
+{
+ lsel.options.remove(selected[i]);
+}
+}
+
