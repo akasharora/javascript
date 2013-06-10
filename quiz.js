09e2078499bc80e6c16d852d9660a793d1160a76
akasharora <akasharora90@gmail.com>
2013-06-10 16:40:05 +0530

    quiz

--- quiz.js
+++ quiz.js
@@ -1,62 +1,62 @@
-function RandomNum() {
-    var var1 = Math.floor((Math.random() * 20)+1);
-    return var1;
+function RandomNumber() {
+    var variable = Math.floor((Math.random() * 20)+1);
+    return variable;
 }
 
 function RandomOperator() {
-    var operator = ['+', '-', '/', '*'];
+    var operators = ['+', '-', '/', '*'];
     var op = Math.floor((Math.random() * 4));
-    return operator[op];
+    return operators[op];
 }
 
-var v1 = [];
-var v2 = [];
-var oper = [];
+var variable1 = [];
+var variable2 = [];
+var operator = [];
 var JSONObject = [];
 
 
 for(var i = 0; i < 20; i++) {
-    v1[i] = RandomNum();
-    v2[i] = RandomNum();
-    oper[i] = RandomOperator();
+    variable1[i] = RandomNumber();
+    variable2[i] = RandomNumber();
+    operator[i] = RandomOperator();
 
     JSONObject[i] = {
-        "var1":v1[i],
-        "var2":v2[i],
-        "op":oper[i],
+        "var1":variable1[i],
+        "var2":variable2[i],
+        "op":operator[i],
         "uo":0,
         "ao":0
     };
     if (JSONObject[i].op == "+") {
-        JSONObject[i].ao = JSONObject[i].var1+JSONObject[i].var2;
+        JSONObject[i].ao = JSONObject[i].var1 + JSONObject[i].var2;
     } else if(JSONObject[i].op == "-") {
-        JSONObject[i].ao = JSONObject[i].var1-JSONObject[i].var2;
+        JSONObject[i].ao = JSONObject[i].var1 - JSONObject[i].var2;
     } else if(JSONObject[i].op == "/") {
-        JSONObject[i].ao = JSONObject[i].var1/JSONObject[i].var2;
+        JSONObject[i].ao = (JSONObject[i].var1 / JSONObject[i].var2).toFixed(2);
     } else if (JSONObject[i].op == "*") {
         JSONObject[i].ao = JSONObject[i].var1 * JSONObject[i].var2;
     }
 }
 
 function Question() {
-    res1 = document.getElementById("result");
-    res1.style.visibility = "visible";
-    res1.innerHTML = "";
+    var res = document.getElementById("result");
+    res.style.visibility = "visible";
+    res.innerHTML = "";
     start1 = document.getElementById("start");
     start1.style.visibility = "hidden";
     next = document.getElementById("next");
     next.style.visibility="visible";
     var x1 = [], x2 = [], x3 = []; 
-    x1 = v1;
-    x2 = v2;
-    x3 = oper;
+    x1 = variable1;
+    x2 = variable2;
+    x3 = operator;
     Display(x1,x2,x3)
 }
 var j=0;
 
-function Display(num1,num2,operator) {
+function Display(num1,num2,operation) {
     var msg1 = document.getElementById("question");
-    msg1.innerHTML = num1[j] + operator[j] + num2[j];
+    msg1.innerHTML = num1[j] + operation[j] + num2[j];
     var msg = document.getElementById("message");
     msg.innerHTML="Question No" + (j+1);
     j = j+1;
@@ -69,43 +69,37 @@ function Display(num1,num2,operator) {
         start.style.visibility = "hidden";
         msg.innerHTML = ""
         DisplayAnswer();
-        
+        return false
     }
 }
 
 function DisplayAns() {
-    var id1 = document.getElementById("result");
-    var id2 = document.getElementById("next");
-    var id3 = document.getElementById("start");
-    var msg1 = document.getElementById("message")
+    var id1 = document.getElementById("div1");
     id1.style.visibility = "hidden";
-    id2.style.visibility = "hidden";
-    id3.style.visibility = "hidden";
 }
 
 var k = 0;
-var sc = 0;
+var score = 0;
 var uresult = [];
 
-function Next() {
+function NextAndEvaluate() {
     uresult = document.getElementById("result").value;
     JSONObject[k].uo = uresult[k];
     if(JSONObject[k].ao == uresult) {
         sc = sc+1;
     }
     k = k+1;
-    score = document.getElementById("score");
-    score.innerHTML = "Your Score : " + sc;
+    score1 = document.getElementById("score");
+    score1.innerHTML = "Your Score : " + score;
     Question();
 }
 
 
 function DisplayAnswer() {
     document.write("Test Finished: Your Results")
-    document.write("<br>" + "your score is " + sc)
+    document.write("<br>" + "your score is " + score)
     for(var p = 0; p < 20; p++) {
-        document.write( "<br>" + (p+1) + "." + "Actual Output= " + JSONObject[p].ao + "   Your Output = " + JSONObject[p].uo);
+        document.write( "<br>" + (p+1) + "." + "Actual Output= " + JSONObject[p].ao +  "     Your Output =  " + JSONObject[p].uo);
     }
-    return false;
+    
 }
-
