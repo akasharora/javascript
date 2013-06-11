55464b2f214900e3719247a2d6aa7958ee056b76
akasharora <akasharora90@gmail.com>
2013-06-11 22:35:28 +0530

    maximum three days selected in a week

--- weekcheck.js
+++ weekcheck.js
@@ -1,29 +1,28 @@
-function MaxThree() {
+var checkboxes = document.getElementById("form1").getElementsByTagName("input");
+
+function maxThree(clicked) {
     var j = 0;
     var checknone = document.getElementById("none");
-    var check = document.getElementById("form1").getElementsByTagName("input");
-    if(checknone.checked) {
+    if (checknone.checked) {
         checknone.checked = false;
         this.checked = true;
     }
 
-    for(var i=0; i<check.length; i++) {
-        if(check[i].checked) {
-            j = j+1;
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            j = j + 1;
         }
 
-        if(j>3) {
-            check[i].checked = false; 
+        if (j > 3) {
+            clicked.checked = false; 
             window.alert("you can't select more than three days");
-            return false; 
+            return false;
             }
        }
 }
 
-
-function SelectNone() {
+function selectNone() {
     var s = document.getElementById("none")
-    var checkboxes = document.getElementById("form1").getElementsByTagName("input");
     for(var i = 0; i < checkboxes.length; i++) {
         checkboxes[i].checked = false;
     }
