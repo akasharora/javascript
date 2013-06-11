0b442dc89235b3572e0360bbb80bcba45f47717b
akasharora <akasharora90@gmail.com>
2013-06-11 18:03:43 +0530

    input name and greet user

--- hellouser.js
+++ hellouser.js
@@ -1,11 +1,18 @@
 var msg = document.getElementById("hello");
-var fname = prompt("please enter your first name");
-var lname = prompt("please enter your last name");
-var op="";
-if(fname && lname) {
-    var output = "hello " + fname + " "+lname+ ", how are you?" ;
+var firstname = prompt("please enter your first name");
+var name_regex = /^[a-z]+$/;
+if (name_regex.test(firstname)) {
+    var lastname = prompt("please enter your last name");
+    if (name_regex.test(lastname)) {
+        alert("hello " + firstname + " " + lastname);
+        var output = "hello " + firstname + " " + lastname + ", how are you?";
+        msg.textContent = output;
+    } else {
+    alert("please try again and enter your last name");
+    }
+} else {
+    alert("please enter your first name");
 }
-else {
-    output = "please try again and enter your full name";
-}
-msg.textContent = output;
+
+
+
