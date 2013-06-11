0c45f8cb0187d6c1bcc1c6eac5ce41cdcd2c58cb
akasharora <akasharora90@gmail.com>
2013-06-11 18:20:22 +0530

    input url and open it

--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
-
-var url=prompt("please enter url you want to open");
+var url = prompt("please enter url you want to open");
 if (url == "") {
     alert("you have entered an empty url, please try again !!");
-}
-else {
-    var win = window.open(url,"new win","width=400px,height=450px,resizable=0,scrollable=0");
+} else {
+    var new_window = window.open(url, "new window","width = 400px,height = 450px,resizable = no,scrollbars = no,status = no,titlebar = no,toolbar = no,menubar = no");
 }
