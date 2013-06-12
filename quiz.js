b598366b726e63693ce3c69a17284a77a1a17f94
akasharora <akasharora90@gmail.com>
2013-06-12 21:39:12 +0530

    quiz

--- quiz.js
+++ quiz.js
@@ -1,105 +1,104 @@
-function RandomNumber() {
+function randomNumber() {
     var variable = Math.floor((Math.random() * 20)+1);
     return variable;
 }
 
-function RandomOperator() {
+function randomOperator() {
     var operators = ['+', '-', '/', '*'];
-    var op = Math.floor((Math.random() * 4));
-    return operators[op];
+    var index = Math.floor((Math.random() * 4));
+    return operators[index];
 }
 
-var variable1 = [];
-var variable2 = [];
+var operand1 = [];
+var operand2 = [];
 var operator = [];
 var JSONObject = [];
 
 
 for(var i = 0; i < 20; i++) {
-    variable1[i] = RandomNumber();
-    variable2[i] = RandomNumber();
-    operator[i] = RandomOperator();
+    operand1[i] = randomNumber();
+    operand2[i] = randomNumber();
+    operator[i] = randomOperator();
 
     JSONObject[i] = {
-        "var1":variable1[i],
-        "var2":variable2[i],
-        "op":operator[i],
-        "uo":0,
-        "ao":0
+        "number1":operand1[i],
+        "number2":operand2[i],
+        "operation":operator[i],
+        "user_op":0,
+        "actual_op":0
     };
-    if (JSONObject[i].op == "+") {
-        JSONObject[i].ao = JSONObject[i].var1 + JSONObject[i].var2;
-    } else if(JSONObject[i].op == "-") {
-        JSONObject[i].ao = JSONObject[i].var1 - JSONObject[i].var2;
-    } else if(JSONObject[i].op == "/") {
-        JSONObject[i].ao = (JSONObject[i].var1 / JSONObject[i].var2).toFixed(2);
-    } else if (JSONObject[i].op == "*") {
-        JSONObject[i].ao = JSONObject[i].var1 * JSONObject[i].var2;
+    if (JSONObject[i].operation == "+") {
+        JSONObject[i].actual_op = JSONObject[i].number1 + JSONObject[i].number2;
+    } else if(JSONObject[i].operation == "-") {
+        JSONObject[i].actual_op = JSONObject[i].number1 - JSONObject[i].number2;
+    } else if(JSONObject[i].operation == "/") {
+        JSONObject[i].actual_op = (JSONObject[i].number1 / JSONObject[i].number2).toFixed(2);
+    } else if (JSONObject[i].operation == "*") {
+        JSONObject[i].actual_op = JSONObject[i].number1 * JSONObject[i].number2;
     }
 }
 
-function Question() {
-    var res = document.getElementById("result");
-    res.style.visibility = "visible";
-    res.innerHTML = "";
-    start1 = document.getElementById("start");
-    start1.style.visibility = "hidden";
+function question() {
+    var result = document.getElementById("result");
+    result.style.visibility = "visible";
+    result.innerHTML = "";
+    start_test = document.getElementById("start");
+    start_test.style.visibility = "hidden";
     next = document.getElementById("next");
     next.style.visibility="visible";
-    var x1 = [], x2 = [], x3 = []; 
-    x1 = variable1;
-    x2 = variable2;
-    x3 = operator;
-    Display(x1,x2,x3)
+    display(operand1,operand2,operator)
+   
 }
 var j=0;
 
-function Display(num1,num2,operation) {
-    var msg1 = document.getElementById("question");
-    msg1.innerHTML = num1[j] + operation[j] + num2[j];
-    var msg = document.getElementById("message");
-    msg.innerHTML="Question No" + (j+1);
+function display(num1,num2,operation) {
+    var question = document.getElementById("question");
+    question.innerHTML = num1[j] + operation[j] + num2[j];
+    var message = document.getElementById("message");
+    message.innerHTML="Question No" + (j+1);
     j = j+1;
     if( j >= 21) {
         var result = document.getElementById("result");
         var next = document.getElementById("next");
-        var start = document.getElementById("start");
+        var start_test = document.getElementById("start");
         result.style.visibility = "hidden";
         next.style.visibility = "hidden";
-        start.style.visibility = "hidden";
-        msg.innerHTML = ""
-        DisplayAnswer();
+        start_test.style.visibility = "hidden";
+        message.innerHTML = ""
+        displayAnswer();
         return false
     }
 }
 
-function DisplayAns() {
-    var id1 = document.getElementById("div1");
-    id1.style.visibility = "hidden";
+function displayAns() {
+    var matter = document.getElementById("display");
+    matter.style.visibility = "hidden";
 }
 
 var k = 0;
 var score = 0;
-var uresult = [];
+var user_result = [];
 
-function NextAndEvaluate() {
-    uresult = document.getElementById("result").value;
-    JSONObject[k].uo = uresult[k];
-    if(JSONObject[k].ao == uresult) {
-        sc = sc+1;
+function nextAndEvaluate() {
+    
+    user_result[k] = document.getElementById("result").value;
+    JSONObject[k].user_op = user_result[k];
+    if(JSONObject[k].actual_op == user_result[k]) {
+        score = score + 1;
     }
     k = k+1;
-    score1 = document.getElementById("score");
-    score1.innerHTML = "Your Score : " + score;
-    Question();
+    var score_msg = document.getElementById("score");
+    score_msg.innerHTML = "Your Score : " + score;
+    question();
 }
 
 
-function DisplayAnswer() {
+function displayAnswer() {
     document.write("Test Finished: Your Results")
     document.write("<br>" + "your score is " + score)
     for(var p = 0; p < 20; p++) {
-        document.write( "<br>" + (p+1) + "." + "Actual Output= " + JSONObject[p].ao +  "     Your Output =  " + JSONObject[p].uo);
+        document.write( "<br/>" + (p+1) +  "." + "Actual Output= " + JSONObject[p].actual_op +  "     Your Output =  " + JSONObject[p].user_op);
     }
+    return false;
     
 }
