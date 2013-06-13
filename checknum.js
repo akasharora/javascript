404251ba96ccb03a529ccf40c15ce686a9902e5e
akasharora <akasharora90@gmail.com>
2013-06-13 13:34:23 +0530

    check value entered is a number or not

--- checknum.js
+++ checknum.js
@@ -1,7 +1,7 @@
 function checkValue() {
     var number = document.getElementById("userinput");
     var result = document.getElementById("result");
-    var number_regex = /^[0-9]+$/;
+    var number_regex = /^[\d+]/;
     if (!number_regex.test(number.value)) {
         result.value = "false";
         alert("you have entered wrong value");
