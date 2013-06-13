6661afaa3cb434cd480c8bc059357a3cbf5d3d92
akasharora <akasharora90@gmail.com>
2013-06-13 11:14:38 +0530

    check all checkboxes or none

--- checkallornone.js
+++ checkallornone.js
@@ -1,6 +1,6 @@
 var checkboxes = document.getElementById("form1").getElementsByTagName("input");
 
-function checkbox(value) {
+function selectCheckbox(value) {
     for (var i = 0; i < checkboxes.length; i++) {
         if (value) {
             checkboxes[i].checked = true;
