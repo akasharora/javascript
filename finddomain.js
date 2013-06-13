97a6f709223de771ae1277b08c2985b14fc9ac5b
akasharora <akasharora90@gmail.com>
2013-06-13 16:21:21 +0530

    extract domain and subdomain from a url

--- finddomain.js
+++ finddomain.js
@@ -1,13 +1,13 @@
-function domainSubdomain() {
+function findDomain() {
     var url = document.getElementById("url").value;
     var domain_subdomain = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
     var domain_regex = /(\w+)\.(\w+)$/;
     var subdomain_regex = /^((\w+\.)*)+(\w+)\.(\w+)$/;
-    var domain = domain_subdomain[1].match(domain_regex)
-    var subdomain = domain_subdomain[1].match(subdomain_regex)
-    if(subdomain[1]) {
-        alert("your domain is " + domain[0] + " and your subdomain is " + subdomain[1])
-    } else {
-        alert("your domain is " + domain[0])
+    var domain = domain_subdomain[1].match(domain_regex);
+    var subdomain = domain_subdomain[1].match(subdomain_regex);
+    var msg = "";
+    if (subdomain[1]) {
+        msg = " subdomain: " + subdomain[1];    
     }
+    alert("your domain: " + domain[0] + msg)
 }
