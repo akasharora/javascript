a8eabb0d3d39ab68648ca732132438fc5d5801b6
akasharora <akasharora90@gmail.com>
2013-06-13 11:23:44 +0530

    maximum three checkboxes selected

--- weekcheck.js
+++ weekcheck.js
@@ -1,18 +1,16 @@
 var checkboxes = document.getElementById("form1").getElementsByTagName("input");
 
-function maxThree(clicked) {
+function maxThreeSelected(clicked) {
     var j = 0;
     var checknone = document.getElementById("none");
     if (checknone.checked) {
         checknone.checked = false;
         this.checked = true;
     }
-
     for (var i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             j = j + 1;
         }
-
         if (j > 3) {
             clicked.checked = false; 
             window.alert("you can't select more than three days");
