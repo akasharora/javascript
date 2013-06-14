1628aaa3c6c712f1f89ed3a93c9b17fdbdce9452
akasharora <akasharora90@gmail.com>
2013-06-14 10:50:13 +0530

    parent and children checkboxes

--- childrencheckbox.js
+++ childrencheckbox.js
@@ -1,12 +1,15 @@
 function displayChildren(clicked_id) {
     var checkboxes = document.getElementById(clicked_id).getElementsByTagName("input")
-    var list = document.getElementById(clicked_id).getElementsByTagName("ul")
+    var list = document.getElementById(clicked_id).getElementsByTagName("div")
     if (checkboxes[0].checked) {
-        list[0].style.visibility = "visible"
+        list[0].style.display = "inherit"
+        
+        document.getElementById("container").scrollTop = document.getElementById(clicked_id).offsetTop;
     } else {
-        list[0].style.visibility = "hidden"
+        list[0].style.display = "none"
         for (var i = 1; i < checkboxes.length; i++) {
-            checkboxes[i].checked = false ;       
-				}
+            checkboxes[i].checked = false;       
+        }
     }
 }
+
