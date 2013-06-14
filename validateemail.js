afe997293316d5fc03783f76d607aac8dff3f22e
akasharora <akasharora90@gmail.com>
2013-06-14 11:47:36 +0530

    validate email and url

--- validateemail.js
+++ validateemail.js
@@ -1,4 +1,4 @@
-function checkForm() {
+function validateForm() {
     var email = document.getElementById("email");
     var homepage = document.getElementById("homepage");
     var email_regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -12,9 +12,9 @@ function checkForm() {
         msg = "Enter correct homepage";
     }
     if (msg != "") {
-       alert(msg);
-       return false;
-    } else { 
-        alert("form validated")
+        alert(msg);
+        return false;
+    } else {
+        alert("form validated");
     }
 }
