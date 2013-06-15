2f49784c97c78c9efb11c3e6b6e0b1bbff1cd640
akasharora <akasharora90@gmail.com>
2013-06-15 13:07:17 +0530

    validate all fieds of the html form

--- validateform.js
+++ validateform.js
@@ -3,29 +3,27 @@ function validateForm() {
     var email = document.getElementById("email");
     var name = document.getElementById("name");
     var homepage = document.getElementById("homepage");
-    var textar = document.getElementById("text_area");
+    var text_area = document.getElementById("text_area");
     var checkbox = document.getElementById("check");
     var email_regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var homepage_regex = /^(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])$/;
-    var msg = "";
+    var message = "";
 
     if(login.value == "") {
-        msg = "login Id can't be empty";
+        message = "login Id can't be empty";
     } else if (!email_regex.test(email.value)) {
-        msg = "enter correct email";
+        message = "enter correct email";
     } else if (name.value == "") {
-        msg = "name field can't be empty";
+        message = "name field can't be empty";
     } else if (!homepage_regex.test(homepage.value)){
-        msg = "you have entered an invalid homepage";
-    } else if (textar.value.length < 50){
-        msg = "information is not complete: should be minimum 50 characters";
+        message = "you have entered an invalid homepage";
+    } else if (text_area.value.length < 50){
+        message = "information is not complete: should be minimum 50 characters";
     }
 
-    if (msg != "") {
-        alert(msg);
+    if (message) {
+        alert(message);
         return false;
-    } else {
-        return true;
     }
 }
 
