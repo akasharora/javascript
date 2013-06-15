c08e4e8906360375383c59d4ff73474a26e75d31
akasharora <akasharora90@gmail.com>
2013-06-15 11:57:08 +0530

    parent and children checkboxes

--- childrencheckbox.js
+++ childrencheckbox.js
@@ -1,10 +1,10 @@
 function displayChildren(clicked_id) {
-    var checkboxes = document.getElementById(clicked_id).getElementsByTagName("input")
-    var list = document.getElementById(clicked_id).getElementsByTagName("div")
+    var checkboxes = document.getElementById(clicked_id).getElementsByTagName("input");
+    var list = document.getElementById(clicked_id).getElementsByTagName("div");
     if (checkboxes[0].checked) {
         list[0].style.display = "inherit"
         
-        document.getElementById("container").scrollTop = document.getElementById(clicked_id).offsetTop;
+        document.getElementById("container").scrollTop = document.getElementById(clicked_id).offsetTop - 5;
     } else {
         list[0].style.display = "none"
         for (var i = 1; i < checkboxes.length; i++) {
